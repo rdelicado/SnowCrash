80f2554ae340a696b1133bed685cfb0a63500074
rdelicad <rdelicad@gmail.com>
2025-04-25 19:23:26 +0200

    flag00

---
+++ level00/resources/cesar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av) 
+{
+    if (ac != 2) {
+        printf("Introduce un texto para descifrar");
+        exit(1);
+    }
+    
+    char c;
+    for (int rot = 0; rot <= 25; rot++) {
+        int i = 0;
+        printf("rot %d -> ", rot );
+        while ((c = av[1][i++])) {
+            if (c >= 'a' && c <= 'z')
+                c = ((c - 'a' + rot) % 26) + 'a';
+            else if (c >= 'A' && c <= 'Z')
+                c = ((c - 'A' + rot) % 26) + 'A';
+            putchar(c);
+        }
+        putchar('\n');
+    }
+   return 0;
+}
